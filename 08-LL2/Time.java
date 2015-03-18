dff94d2990d3b3a435c555185387a1493e2bafb5
kaytrean <kaytrean@gmail.com>
2015-03-18 09:08:56 -0400

    Create Time.java

---
+++ 08-LL2/Time.java
@@ -0,0 +1,12 @@
+import java.io*; 
+import java.util*;
+
+public class Time {
+  public static void main(String[] args) {
+    long start,elapsed;
+    
+    start = System.currentTimeMillis();
+    //add some other stuff
+    elapsed = System.current TimeMillis()- start;
+    }
+}
