ff54a8b515d9e05e092a922d5631c7ba9d82f5c0
kaytrean <kaytrean@gmail.com>
2015-03-26 08:54:18 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+    private Node<E> previous;
+
+    public Node() {
+        data = null;
+        next = null;
+        previous = null;
+    }
+
+    public Node(E s) {
+        data = s;
+        next = null;
+        previous = null;
+    }
+
+    public void setData(E s) {
+        data = s;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setNext(Node<E> s) {
+        next = s;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public String toString() {
+        return ""+data;
+    }
+
+    public void setPrevious(Node<E> s) {
+        previous = s;
+    }
+
+    public Node<E> getPrevious() {
+        return previous;
+    }
+}
