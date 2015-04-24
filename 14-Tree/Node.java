0912817852adc9385f5126b947997ff3f06e2af7
kaytrean <kaytrean@gmail.com>
2015-04-24 08:56:35 -0400

    Create Node.java

---
+++ 14-Tree/Node.java
@@ -0,0 +1,21 @@
+public class Node{
+  private int data;
+  private Node left, right;
+  
+  public Node getLeft(){
+    return left;
+  }
+  
+  public void setLeft(Node n){
+    left = n;
+  }
+    
+  public Node getRight(){
+    return right;
+  }
+  
+  public void setRight(Node n){
+    right = n;
+    }
+    
+}
